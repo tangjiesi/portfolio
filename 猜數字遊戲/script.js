8bb85f11b7bad77b76088ad8b63d1759302e6d59
jiesitang <79500960+jiesitang@users.noreply.github.com>
2024-11-18 16:40:22 +0800

    Add files via upload

---
+++ 猜數字遊戲/script.js
@@ -0,0 +1,41 @@
+'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').style.width = '25rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber && score > 1) {
+    score--;
+    document.querySelector('.score').textContent = score;
+    if (guess > secretNumber) {
+      document.querySelector('.message').textContent = 'Too high!';
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = 'Too low!';
+    }
+  } else {
+    document.querySelector('.score').textContent = 0;
+    document.querySelector('.message').textContent = 'You lose!';
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
